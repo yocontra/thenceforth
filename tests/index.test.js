9aa89b6c9a01104bdedc66501d521ab4cee9f1b3
Steve Lacy <me@slacy.me>
2018-11-29 19:38:42 -0700

    Add tests for pluralization

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,11 +6,17 @@ test('Date() should return now', () => {
 test('Date() - 59s should return "Just now"', () => {
   expect(thenceforth(new Date().getTime() - 59000)).toBe('Just now')
 })
-test('Date() - 1 minute should return "1 minute ago"', () => {
+test('Date() - 1 minute + 10s should return "1 minute ago"', () => {
   expect(thenceforth(new Date().getTime() - 70000)).toBe('1 minute ago')
+})
+test('Date() - 1 minute should return "1 minute ago"', () => {
+  expect(thenceforth(new Date().getTime() - 60000)).toBe('1 minute ago')
 })
 test('Date() - 20 minute should return "20 minutes ago"', () => {
   expect(thenceforth(new Date().getTime() - 60000 * 20)).toBe('20 minutes ago')
+})
+test('Date() - 1 hour should return "1 hour ago"', () => {
+  expect(thenceforth(new Date().getTime() - 60000 * 60)).toBe('1 hour ago')
 })
 test('Date() - 10 hours should return "10 hours ago"', () => {
   expect(thenceforth(new Date().getTime() - 60000 * 60 * 10)).toBe('10 hours ago')
@@ -23,6 +29,9 @@ test('Date() - 10.75 hours should return "11 hours ago"', () => {
 })
 test('Date() - 4 days should return "4 days ago"', () => {
   expect(thenceforth(new Date().getTime() - 60000 * 60 * 24 * 4)).toBe('4 days ago')
+})
+test('Date() - 1 day should return "Yesterday"', () => {
+  expect(thenceforth(new Date().getTime() - 60000 * 60 * 24)).toBe('Yesterday')
 })
 test('Date() - 2 days should return "Yesterday"', () => {
   expect(thenceforth(new Date().getTime() - 60000 * 60 * 24 * 1)).toBe('Yesterday')
